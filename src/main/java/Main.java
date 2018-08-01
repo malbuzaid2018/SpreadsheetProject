b64d291e5e941e8c4cc2c91304be1e57b1ae3588
malbuzaid2018 <39639309+malbuzaid2018@users.noreply.github.com>
2018-08-01 20:07:27 +0700

    test methods in TheTimeMap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -36,5 +36,20 @@ public class Main {
             System.out.println(entry.getValue().getDate() + " " + entry.getValue().getTime() + " : Min = " + entry.getValue().getMinimumRequired());
         }
 
+        System.out.println("------------------------------------------------");
+
+        //test methods in TheTimeMap
+
+        System.out.println("\nTest displaySchedule:");
+        schedule.displaySchedule();
+
+        System.out.println("\nTest displayPeopleWorking:");
+        schedule.displayPeopleWorking("9:30 Mon 7/2");
+
+        System.out.println("\nTest displayPeopleAvailable:");
+        schedule.displayPeopleAvailable("9:30 Mon 7/2");
+
+        System.out.println("\nTest displayMinAndMax:");
+        schedule.displayMinAndMax("9:30 Mon 7/2");
     }
 }
